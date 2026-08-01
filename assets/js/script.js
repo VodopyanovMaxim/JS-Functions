b93b301e8f01b4e4007fb27ce148e6e6648831ac
maksymvodop <maximvodopanov@gmail.com>
2026-08-01 16:08:40 +0300

    Add menu to select functions

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+let loop = true;
+while (loop) {
+  let choice = Number(prompt("Виберіть номер задачі для тестування"));
+  switch (choice) {
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    default:
+      loop = false;
+      break;
+  }
+}
