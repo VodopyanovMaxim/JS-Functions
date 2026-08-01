3f70e93efe541f3fc3d4a24a68fe1c6e82883898
maksymvodop <maximvodopanov@gmail.com>
2026-08-01 16:28:12 +0300

    Add first task script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,34 @@
+function isWorkingAgePerson(age) {
+  if (age < 64 && age > 16) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 let loop = true;
 while (loop) {
-  let choice = Number(prompt("Виберіть номер задачі для тестування"));
+  const choice = Number(
+    prompt(`
+    Виберіть номер задачі для тестування:\n
+       1. Перевірка працездатного віку\n
+       2. Перевірка кратності чисел\n
+       3. Реконструкція парку: Розрахунок матеріалів`),
+  );
   switch (choice) {
     case 1:
+      const age = Number(
+        prompt("Введіть вік для перевірки на те, чи є він працездатним:"),
+      );
+      if (typeof age !== NaN && age > 0) {
+        if (isWorkingAgePerson(age)) {
+          alert(`Людина віком ${age} є працездатною`);
+        } else {
+          alert(`Людина віком ${age} не є працездатною`);
+        }
+      } else {
+        alert(`Помилка, введіть коректне значення`);
+      }
       break;
     case 2:
       break;
